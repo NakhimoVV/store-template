48a3b5e9bcb119068a068ad5b9f9bf8ee81b6eaa
Aleksey Nakhimov <tj1993@bk.ru>
2025-05-28 16:40:47 +0300

    third commit

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__inner container">Logo Navigation Actions</div>
+    </header>
+  )
+}
+
+export default Header
